1e3d8eee12f0ae8d4faa6b18879877b2463cf27e
maureen-james <manjax97@gmail.com>
2022-05-18 13:28:32 +0300

    modify all templates

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,115 @@
 from flask import render_template,redirect,url_for,abort,request,flash
 from app.models import User
-from .forms import UpdateProfile
 from .. import db
 from flask_login import login_required,current_user
 from app.main import main
+from .forms import FeedbackForm, CommentForm, UpdateProfile
+from ..models import Feedback, Comment, User, Likes, Dislikes
+
+@main.route('/')
+def index():
+    pitches = Pitch.query.order_by(Pitch.date_created).all()
+    
+    return render_template('index.html',pitches=pitches)
+
+
+@main.route('/new_pitches', methods = ['POST','GET'])
+@login_required
+def new_pitch():
+    form = FeedbackForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        post = form.post.data
+        category = form.category.data
+        user_id = current_user
+        new_pitch_object = Pitch(post=post,user_id=current_user._get_current_object().id,category=category,title=title)
+        new_pitch_object.save()
+        return redirect(url_for('main.index'))
+        
+    return render_template('new_pitch.html', form = form)
+
+@main.route('/comment/<int:pitch_id>', methods = ['POST','GET'])
+@login_required
+def comment(pitch_id):
+    form = CommentForm()
+    pitch = Pitch.query.get(pitch_id)
+    all_comments = Comment.query.filter_by(pitch_id = pitch_id).all()
+    if form.validate_on_submit():
+        comment = form.comment.data 
+        pitch_id = pitch_id
+        user_id = current_user._get_current_object().id
+        new_comment = Comment(
+            comment = comment,
+            user_id = user_id,
+            pitch_id = pitch_id)
+        new_comment.save_c()
+        return redirect(url_for('.comment', pitch_id = pitch_id))
+    return render_template('comment.html', form =form, pitch = pitch,all_comments=all_comments)
+
+
+@main.route('/user/<name>')
+def profile(name):
+    user = User.query.filter_by(username = name).first()
+    user_id = current_user._get_current_object().id
+    posts = Pitch.query.filter_by(user_id = user_id).all()
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user,posts=posts)
+
+@main.route('/user/<name>/updateprofile', methods = ['POST','GET'])
+@login_required
+def updateprofile(name):
+    form = UpdateProfile()
+    user = User.query.filter_by(username = name).first()
+    if user == None:
+        abort(404)
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        user.save_u()
+        return redirect(url_for('.profile',name = name))
+    return render_template('profile/update.html',form =form)
+
+
+# @main.route('/user/<name>/update_pic',methods= ['POST'])
+# @login_required
+# def update_pic(name):
+#     user = User.query.filter_by(username = name).first()
+#     if 'photo' in request.files:
+#         filename = photos.save(request.files['photo'])
+#         path = f'photos/{filename}'
+#         user.profile_pic_path = path
+#         db.session.commit()
+#     return redirect(url_for('main.profile',name=name))
+
+@main.route('/like/<int:id>',methods = ['POST','GET'])
+@login_required
+def like(id):
+    get_pitches = Likes.get_likes(id)
+    valid_string = f'{current_user.id}:{id}'
+    for pitch in get_pitches:
+        to_str = f'{pitch}'
+        print(valid_string+" "+to_str)
+        if valid_string == to_str:
+            return redirect(url_for('main.index',id=id))
+        else:
+            continue
+    new_vote = Likes(user = current_user, pitch_id=id)
+    new_vote.save()
+    return redirect(url_for('main.index',id=id))
+
+@main.route('/dislike/<int:id>',methods = ['POST','GET'])
+@login_required
+def dislike(id):
+    pitch = Dislikes.get_dislikes(id)
+    valid_string = f'{current_user.id}:{id}'
+    for p in pitch:
+        to_str = f'{p}'
+        print(valid_string+" "+to_str)
+        if valid_string == to_str:
+            return redirect(url_for('main.index',id=id))
+        else:
+            continue
+    new_dislike = Dislikes(user = current_user, pitch_id=id)
+    new_dislike.save()
+    return redirect(url_for('main.index',id = id))
