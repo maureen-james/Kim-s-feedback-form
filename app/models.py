5ef414f8dc08b6cc7f5b0b35e496d06527635dcf
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-05-18 15:21:58 +0300

    Updates models

--- app/models.py
+++ app/models.py
@@ -3,40 +3,24 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 
-#New login changes.
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class Feedback(db.Model):
-    
-    __tablename__ = 'feedback'
+class User(UserMixin,db.Model):
 
-    id = db.Column(db.Integer,primary_key=True)
-    company = db.Column(db.String)
-    posted = db.Column(db.DateTime,default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    likes = db.relationship('Likes',backref='post',lazy='dynamic')
-    dislikes = db.relationship('Dislikes',backref='post',lazy='dynamic')
-    comment = db.relationship('Comment',backref='post',lazy='dynamic')
-
-    def save_feedback(self):
-        db.session.add(self)
-        db.session.commit()
-
-    def _repr_(self):
-        return f'Feedback{self.company}'
-class User(UserMixin, db.Model):
-    
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255),unique = True)
+    username = db.Column(db.String(100))
     bio = db.Column(db.String(255))
+    email = db.Column(db.String(100),nullable = False,unique = True)
     avatar = db.Column(db.String())
-    email = db.Column(db.String(255),unique= True,index = True)
     password_hash = db.Column(db.String(255))
     password_secure = db.Column(db.String(255))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    feedback = db.relationship('Feedback', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
 
     @property
     def password(self):
@@ -54,72 +38,28 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return f'User {self.username}'
 
-class Comment(db.Model):
+class Feedback(db.Model):
     
-    __tablename__ = 'comments'
+    __tablename__ = 'feedback'
 
     id = db.Column(db.Integer,primary_key=True)
-    title = db.Column(db.String)
-    feedback_id = db.Column(db.Integer,db.ForeignKey('feedback.id'))
-    feedback_comment = db.Column(db.String)
-    posted = db.Column(db.DateTime,default = datetime.utcnow)
-    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
-
-    def save_comment(self):
+    company = db.Column(db.String)
+    context = db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    upvote = db.relationship('Upvote',backref='post',lazy='dynamic')
+    downvote = db.relationship('Downvote',backref='post',lazy='dynamic')
+    comment = db.relationship('Comment',backref='post',lazy='dynamic')
+    delete = db.relationship('Delete',backref = 'post',lazy='dynamic')
+ 
+    def save_feedback(self):
         db.session.add(self)
         db.session.commit()
-
-    @classmethod
-    def get_comments(cls, feedback_id):
-        comments = Comment.query.filter_by(feedback_id = feedback_id).all()
-        return comments
-
-    @classmethod
-    def get_comment_writer(cls, user_id):
-        writer = User.query.filter_by(id=user_id).first()
-
-        return writer
-
-class Dislikes(db.Model):
-    _tablename_ = 'dislikes'
-
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    feedback_id = db.Column(db.Integer, db.ForeignKey('feedback.id'))
-
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
-
-    @classmethod
-    def get_downvotes(cls, id):
-        dislikes = Dislikes.query.filter_by(feedback_id = id).all()
-        return dislikes
 
     def _repr_(self):
-        return f'{self.user_id}:{self.feedback_id}'
-
-class Likes(db.Model):
-    _tablename_ = 'likes'
-
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    feedback_id = db.Column(db.Integer, db.ForeignKey('feedback.id'))
-
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
-
-    @classmethod
-    def get_upvotes(cls, id):
-        likes = Likes.query.filter_by(feedback_id=id).all()
-        return likes
-
-    def _repr_(self):
-        return f'{self.user_id}:{self.feedback_id}'
+        return f'Feedback{self.company}'
 
 class Role(db.Model):
-    
+
     __tablename__ = 'roles'
 
     id = db.Column(db.Integer,primary_key = True)
@@ -128,3 +68,91 @@ class Role(db.Model):
 
     def __repr__(self):
         return f'User {self.name}'
+
+class Upvote(db.Model):
+
+    _tablename_ = 'upvotes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    feedback_id = db.Column(db.Integer, db.ForeignKey('feedback.id'))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_upvotes(cls, id):
+        upvote = Upvote.query.filter_by(feedback_id=id).all()
+        return upvote
+
+    def _repr_(self):
+        return f'{self.feedback_id}'
+
+class Delete(db.Model):
+
+    __tablename__ = 'delete'
+    id = db.Column(db.Integer,primary_key = True)
+    feedback_id = db.Column(db.Integer,db.ForeignKey('feedback.id'))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_deletes(cls,id):
+        delete = Delete.query.filter_by(feedback_id=id).all()
+        return delete
+
+    def _repr_(self):
+        return f'{self.feedback_id}'
+
+class Comment(db.Model):
+    
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key = True)
+    comment = db.Column(db.Text(),nullable = False)
+    feedback_id = db.Column(db.Integer,db.ForeignKey('feedback.id'))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls,feedback_id):
+        comments = Comment.query.filter_by(feedback_id = feedback_id).all()
+
+        return comments
+
+    def __repr__(self):
+        return f'comment:{self.comment}'
+
+        
+class Downvote(db.Model):
+
+    _tablename_ = 'downvotes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    feedback_id = db.Column(db.Integer, db.ForeignKey('feedback.id'))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_downvotes(cls, id):
+        downvote = Downvote.query.filter_by(feedback_id=id).all()
+        return downvote
+
+    def _repr_(self):
+        return f'{self.feedback_id}'
+
+class Quote:
+    '''
+    Class that returns quotes requested from the quote API
+    '''
+    def __init__(self, author, quote):
+        self.author = author
+        self.quote = quote
+
+
