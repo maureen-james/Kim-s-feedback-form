55217108f9b3b6f8bc9b00ea30b7837dd985e445
Kimberly <kimolanga123@gmail.com>
2022-05-18 15:50:58 +0300

    add main route

--- app/main/views.py
+++ app/main/views.py
@@ -1,115 +1,151 @@
-from flask import render_template,redirect,url_for,abort,request,flash
-from app.models import User
-from .. import db
-from flask_login import login_required,current_user
-from app.main import main
-from .forms import FeedbackForm, CommentForm, UpdateProfile
-from ..models import Feedback, Comment, User, Likes, Dislikes
+from . import main
+from flask import render_template,abort,redirect,url_for,request,flash
+from flask_login import login_required
+from ..models import User,Feedback,Comment,Upvote,Downvote
+from .. import db,photos
+from .forms import UpdateProfile, FeedbackForm, CommentForm
+from ..requests import get_quote
+
+#Application views
+@main.route('/dashboard')
+def dashboard():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title = 'Kim-s-feedback-form'
+    user = User.query.all()
+    quotes = get_quote()
+    feedback = Feedback.query.all()
+
+    return render_template ('index.html',title=title,feedback=feedback,quotes=quotes,user=user)
 
 @main.route('/')
 def index():
-    pitches = Pitch.query.order_by(Pitch.date_created).all()
-    
-    return render_template('index.html',pitches=pitches)
+    '''
+    View root page function that returns the index page and its data
+    '''
 
-
-@main.route('/new_pitches', methods = ['POST','GET'])
-@login_required
-def new_pitch():
-    form = FeedbackForm()
-    if form.validate_on_submit():
-        title = form.title.data
-        post = form.post.data
-        category = form.category.data
-        user_id = current_user
-        new_pitch_object = Pitch(post=post,user_id=current_user._get_current_object().id,category=category,title=title)
-        new_pitch_object.save()
-        return redirect(url_for('main.index'))
-        
-    return render_template('new_pitch.html', form = form)
-
-@main.route('/comment/<int:pitch_id>', methods = ['POST','GET'])
-@login_required
-def comment(pitch_id):
-    form = CommentForm()
-    pitch = Pitch.query.get(pitch_id)
-    all_comments = Comment.query.filter_by(pitch_id = pitch_id).all()
-    if form.validate_on_submit():
-        comment = form.comment.data 
-        pitch_id = pitch_id
-        user_id = current_user._get_current_object().id
-        new_comment = Comment(
-            comment = comment,
-            user_id = user_id,
-            pitch_id = pitch_id)
-        new_comment.save_c()
-        return redirect(url_for('.comment', pitch_id = pitch_id))
-    return render_template('comment.html', form =form, pitch = pitch,all_comments=all_comments)
+    return render_template ('index.html')
 
 
 @main.route('/user/<name>')
 def profile(name):
     user = User.query.filter_by(username = name).first()
-    user_id = current_user._get_current_object().id
-    posts = Pitch.query.filter_by(user_id = user_id).all()
+
+    if user is None:
+        abort(404)
+    else:
+        return render_template('profile/profile.html', user = user)
+
+@main.route('/user/<name>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(name):
+    user = User.query.filter_by(username = name).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.avatar = path
+        db.session.commit()
+    return redirect(url_for('main.profile',name = name))
+
+@main.route('/user/<name>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(name):
+    user = User.query.filter_by(username = name).first()
     if user is None:
         abort(404)
 
-    return render_template("profile/profile.html", user = user,posts=posts)
-
-@main.route('/user/<name>/updateprofile', methods = ['POST','GET'])
-@login_required
-def updateprofile(name):
     form = UpdateProfile()
-    user = User.query.filter_by(username = name).first()
-    if user == None:
-        abort(404)
+
     if form.validate_on_submit():
         user.bio = form.bio.data
-        user.save_u()
-        return redirect(url_for('.profile',name = name))
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',name = user.username))
+
     return render_template('profile/update.html',form =form)
 
-
-# @main.route('/user/<name>/update_pic',methods= ['POST'])
-# @login_required
-# def update_pic(name):
-#     user = User.query.filter_by(username = name).first()
-#     if 'photo' in request.files:
-#         filename = photos.save(request.files['photo'])
-#         path = f'photos/{filename}'
-#         user.profile_pic_path = path
-#         db.session.commit()
-#     return redirect(url_for('main.profile',name=name))
-
-@main.route('/like/<int:id>',methods = ['POST','GET'])
+@main.route('/create_new',methods = ['GET','POST'])
 @login_required
+def new_feedback():
+    form = FeedbackForm()
+
+    if form.validate_on_submit():
+        company = form.company.data
+        context = form.context.data
+        new_feedback = Feedback(company=company,context=context)
+        
+        db.session.add(new_feedback)
+        db.session.commit()
+        return redirect(url_for('main.index'))
+    else:
+        all_feedback = Feedback.query.order_by(Feedback.posted)
+
+    return render_template('new_feedback.html',feedback_form = form,feedback=all_feedback)
+
+@main.route('/comment/<int:feedback_id>', methods = ['POST','GET'])
+def comment(feedback_id):
+    form = CommentForm()
+    feedback = Feedback.query.get(feedback_id)
+    all_comments = Comment.query.filter_by(feedback_id = feedback_id).all()
+    if form.validate_on_submit():
+        comment = form.comment.data 
+        feedback_id = feedback_id
+        new_comment = Comment(comment=comment,feedback_id = feedback_id)
+        db.session.add(new_comment)
+        db.session.commit()
+        return redirect(url_for('.comment', feedback_id = feedback_id))
+    return render_template('comment.html',comment_form =form, feedback = feedback,all_comments=all_comments)
+
+@main.route('/like/<int:id>', methods=['GET', 'POST'])
 def like(id):
-    get_pitches = Likes.get_likes(id)
-    valid_string = f'{current_user.id}:{id}'
-    for pitch in get_pitches:
-        to_str = f'{pitch}'
-        print(valid_string+" "+to_str)
-        if valid_string == to_str:
-            return redirect(url_for('main.index',id=id))
-        else:
-            continue
-    new_vote = Likes(user = current_user, pitch_id=id)
-    new_vote.save()
-    return redirect(url_for('main.index',id=id))
+    feedback = Feedback.query.get(id)
+    if feedback is None:
+        abort(404)
+    like = Upvote.query.filter_by(feedback_id=id).first()
+    if like is not None:
+        db.session.add(like)
+        db.session.commit()
+    new_like = Upvote(feedback_id=id)
+    db.session.add(new_like)
+    db.session.commit()
+    return redirect(url_for('main.index'))
 
-@main.route('/dislike/<int:id>',methods = ['POST','GET'])
-@login_required
+
+@main.route('/dislike/<int:id>', methods=['GET', 'POST'])
 def dislike(id):
-    pitch = Dislikes.get_dislikes(id)
-    valid_string = f'{current_user.id}:{id}'
-    for p in pitch:
-        to_str = f'{p}'
-        print(valid_string+" "+to_str)
-        if valid_string == to_str:
-            return redirect(url_for('main.index',id=id))
-        else:
-            continue
-    new_dislike = Dislikes(user = current_user, pitch_id=id)
-    new_dislike.save()
-    return redirect(url_for('main.index',id = id))
+    feedback = Feedback.query.get(id)
+    if feedback is None:
+        abort(404)
+    
+    dislike = Downvote.query.filter_by(feedback_id=id).first()
+    
+    if dislike is not None:   
+        db.session.add(dislike)
+        db.session.commit()
+
+    new_dislike = Downvote(feedback_id=id)
+    db.session.add(new_dislike)
+    db.session.commit()
+    
+    return redirect(url_for('main.index'))
+
+@main.route('/delete_feedback/<int:id>',methods = ['GET','POST'])
+@login_required
+def delete(id):
+    feedback= Feedback.query.get(id)
+    db.session.delete(feedback)
+    db.session.commit()
+
+    return redirect(url_for('main.index'))
+
+@main.route("/delete_comment/<int:id>")
+@login_required
+def delete_comment(id):
+    comment = Comment.query.get(id)
+    db.session.delete(comment)
+    db.session.commit()
+
+    return redirect(url_for('main.index'))
